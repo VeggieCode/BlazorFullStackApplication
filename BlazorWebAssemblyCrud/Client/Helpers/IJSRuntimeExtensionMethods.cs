44b36f3ad3733c9c48707b7ac0baef0cf8033d0d
Alexis Alvarez <alexisao@hotmail.com>
2021-07-10 01:28:23 -0500

    Creación de la clase IJSRuntimeExtensionMethods
     *Esta clase es una intermediaria entre el código javascript de tal manera que cuando se requiera llamar una invocación de un método JavaScript, no se tenga que invocar directamente con JSInterop. En cambio, se manda a llamar con código C# a la clase IJSRRuntimeExtensionMethods
    *Métodos creados:
      - Todos los métodos que se encuentran en Utils.js, encargados de acceder y modificar los registros de las indexDB

---
+++ BlazorWebAssemblyCrud/Client/Helpers/IJSRuntimeExtensionMethods.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.JSInterop;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace BlazorWebAssemblyCrud.Client.Helpers
+{
+    public static class IJSRuntimeExtensionMethods
+    {
+        //Se necesita encapsular los registros obtenidos del indexDB en una clase
+        public static async ValueTask<RegistersDBLocal> GetPendingRegister(this IJSRuntime js)
+        {
+            return await js.InvokeAsync<RegistersDBLocal>("getPendingRegisters");
+        }
+
+        public static async ValueTask<int> GetPendingRegistersCount(this IJSRuntime js)
+        {
+            return await js.InvokeAsync<int>("getPendingRegistersCount");
+        }
+        public static async ValueTask DeleteRegister(this IJSRuntime js, string table, int id)
+        {
+            await js.InvokeVoidAsync("deleteRegister", table, id);
+        }
+
+        public static async ValueTask SaveRegisterForCreate<T>(this IJSRuntime js, string url, T entity)
+        {
+            var body = JsonSerializer.Serialize(entity);
+            await js.InvokeVoidAsync("saveRegisterForCreate", url, body);
+        }
+
+        public static async ValueTask SaveRegisterForDelete(this IJSRuntime js, string url)
+        {
+            await js.InvokeVoidAsync("saveRegisterForDelete", url);
+        }
+
+
+    }
+}
