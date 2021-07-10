8fca8aa3bf3e69db38323584fbfb1e6354911471
Alexis Alvarez <alexisao@hotmail.com>
2021-07-10 01:01:10 -0500

    Creación de la clase AppState
    * Esta clase contiene un evento al cual deben suscribirse los componentes que hagan uso de la sincronización de cambios

---
+++ BlazorWebAssemblyCrud/Client/Helpers/AppState.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorWebAssemblyCrud.Client.Helpers
+{
+    public class AppState
+    {        
+        public event Func<Task> OnUpdatePendingSyncs;
+        public async Task NotifyUpdatePendingSyncs() => await OnUpdatePendingSyncs?.Invoke();
+    }
+}
